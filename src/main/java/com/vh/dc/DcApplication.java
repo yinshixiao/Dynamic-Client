f1da0f302ce48c300f7e5f1ee64b66a5ef4d2f8a
yinshixiao <593853565@qq.com>
2018-08-07 21:56:33 +0800

    22

--- src/main/java/com/vh/dc/DcApplication.java
+++ src/main/java/com/vh/dc/DcApplication.java
@@ -1,9 +1,12 @@
 package com.vh.dc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DcApplication {
 
 	public static void main(String[] args) {
